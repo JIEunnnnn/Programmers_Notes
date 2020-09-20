c25376c456143510d50e5067f42b4226f2f6e386
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-09-20 15:29:38 +0900

    Update 모의고사.py

--- BruteForce/모의고사.py
+++ BruteForce/모의고사.py
@@ -1,10 +1,16 @@
-from operator import itemgetter
+# LV.1 모의고사
+# 가장많이 맞추는 수포자 찾기:) 
+#
+# 다차원리스트 정렬 및 append() 메소드 사용
+#
+# 다차원리스트를 정렬시키는 라이브러리 
+from operator import itemgetter 
 
 def solution(answers):
     answer = []
-    a1 = [1,2,3,4,5]
-    a2 = [2,1,2,3,2,4,2,5]
-    a3 = [3,3,1,1,2,2,4,4,5,5]
+    a1 = [1,2,3,4,5]  #수포자1
+    a2 = [2,1,2,3,2,4,2,5] #수포자2 
+    a3 = [3,3,1,1,2,2,4,4,5,5] #수포자3 
     
     i=0
     j=0
@@ -43,7 +49,8 @@ def solution(answers):
         {'key' : 2, 'count' : count_a2},
         {'key': 3, 'count': count_a3 }    
     ] 
-    listmax = sorted(max, key=itemgetter('count'), reverse=True ) 
+    
+    listmax = sorted(max, key=itemgetter('count'), reverse=True ) #count(맞춘갯수)기준으로 내림차순정렬 
     
     sz = 0 
     compare = listmax[sz]['count']
@@ -54,3 +61,6 @@ def solution(answers):
         sz +=1
         
     return answer
+
+
+
