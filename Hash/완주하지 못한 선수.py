e0d330e941609ab05485df3a1f018f917d1c1094
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-09-20 15:49:26 +0900

    Update 완주하지 못한 선수.py

--- Hash/완주하지 못한 선수.py
+++ Hash/완주하지 못한 선수.py
@@ -5,12 +5,12 @@
 
 def solution(participant, completion):
     
-    participant = sorted(participant, reverse=True) #리스트를 오름차순으로 정렬시키기
+    participant = sorted(participant, reverse=True) # 리스트를 오름차순으로 정렬시키기
     completion = sorted(completion, reverse=True)
     answer = '' 
 
     
-    while len(completion) : #완수자 리스트 크기만큼 반복문실행 
+    while len(completion) : # 완수자 리스트 크기만큼 반복문실행 
         compare_com = completion.pop() 
         compare_part = participant.pop()
         
@@ -18,7 +18,7 @@ def solution(participant, completion):
             answer = compare_part
             break 
         
-    if answer == '' :  #완주자만큼 반복문 실행해도 답이없다? 그럼 참여자리스트에 미완수자존재 :)
+    if answer == '' :  # 완주자만큼 반복문 실행해도 답이없다? 그럼 참여자리스트에 미완수자존재 :)
         answer = participant.pop()
         
 
