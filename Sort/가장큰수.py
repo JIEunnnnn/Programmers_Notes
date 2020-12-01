49016bc668f5b72af3b7ff78c6b04ca281273fb1
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-01 18:01:48 +0900

    Update 가장큰수.py

--- Sort/가장큰수.py
+++ Sort/가장큰수.py
@@ -4,10 +4,19 @@
 # 1. permutations쓰면 시간초과발생 -> NO...
 # 2. map() 메소드를 통해 숫자를 문자열(str)로 반환하고 sort() 로 정렬..
 #    문자열을 sort()시 아스키코드로 비교하여 정렬 // 몰랐다ㅠㅠ 
+#
+#"크게 만드는 수"의 기준  
+# 1. 대소관계비교 기준 2. 배열정렬 3. 문자열표현
 
 def solution(numbers):
     num = list(map(str, numbers))
     num.sort(key =lambda x : x*3, reverse= True) #내림차순정렬, *3하는 이유는 숫자들이 1000이하라고 제한조건ㅇㅇ
+    #num.sort(key = lambda x : (X * 4)[:4], reverse = True)
+    # if num[0] == '0' : #정렬하고 난 후... 첫번째시작이 0일경우 == 0
+    #      answer = '0'   
+    # else : 
+    #       answer = ''.join(numbers)
+    # 3 VS 34 => 3333 VS 34343434 [:4] => 333 VS 343 
     #print(num)
     
     return str(int(''.join(num)))
