e3397e1b50fe9e77a00cd1df5acf9036654bb28c
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-01 17:26:23 +0900

    Update 가장큰수.py

--- Sort/가장큰수.py
+++ Sort/가장큰수.py
@@ -1,3 +1,16 @@
+#LV.2 가장큰수
+#주어진 숫자를 조합하여 가장 큰 숫자출력하기 
+#
+# 1. permutations쓰면 시간초과발생 -> NO...
+# 2. map() 메소드를 통해 숫자를 문자열(str)로 반환하고 sort() 로 정렬..
+#    문자열을 sort()시 아스키코드로 비교하여 정렬 // 몰랐다ㅠㅠ 
+
+def solution(numbers):
+    num = list(map(str, numbers))
+    num.sort(key =lambda x : x*3, reverse= True) #내림차순정렬, *3하는 이유는 숫자들이 1000이하라고 제한조건ㅇㅇ
+    #print(num)
+    
+    return str(int(''.join(num)))
 
 
 =====================================================================
