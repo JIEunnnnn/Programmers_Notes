5311ebaa0058dfa23fc69a071a7e1a25c9235012
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-03 17:44:22 +0900

    Update 타겟넘버.py

--- DFS&BFS/타겟넘버.py
+++ DFS&BFS/타겟넘버.py
@@ -2,7 +2,7 @@
 #주어진 numbers 전체수를 +-하여 target 숫자 맞추기
 #
 #진짜어려워서... 다른사람 풀이를 참고함ㅠ 
-# DFS 를 활용하여 각각 +-시키는 구조로 프로그램을 짰다...
+#DFS 를 활용하여 각각 +-시키는 구조로 프로그램을 짰다...
 
 def solution(numbers, target):
     trr = [0]
@@ -12,5 +12,5 @@ def solution(numbers, target):
             sub_trr.append(sub + num) #기준에서 +-num 시키기
             sub_trr.append(sub - num)
         trr = sub_trr
-    answer = trr.count(target)
+    answer = trr.count(target) #찾고자 하는 항목이, 파이썬의 리스트에 몇개나 들어있는지 확인하는 count 함수
     return answer
