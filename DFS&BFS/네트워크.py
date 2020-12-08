e3ada043fcca597eacc5f0fda3701d0949ab4a99
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-08 16:04:20 +0900

    Create 네트워크.py

---
+++ DFS&BFS/네트워크.py
@@ -0,0 +1,20 @@
+=============================================================
+#1차시도
+#return = 전체노드 수 - 연결 간선수 
+#         n - sum(computers)
+import math
+
+def solution(n, computers):
+    sum = 0
+    
+    for i,v in enumerate(computers) : #index
+        for j,k in enumerate(v) :
+            if i != j :
+                sum += k
+                
+
+    #print(sum) 
+    #print(int(sum/2))
+    answer =  n - math.ceil(sum/2) 
+    #answer = 0
+    return answer
