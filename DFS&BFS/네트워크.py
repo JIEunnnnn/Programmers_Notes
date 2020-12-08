96942064d6216d4d2228bf08b5e0ba4dc3e60705
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-08 21:20:42 +0900

    Update 네트워크.py

--- DFS&BFS/네트워크.py
+++ DFS&BFS/네트워크.py
@@ -1,5 +1,34 @@
 # 리트코드 200 
 
+#경로만 알면 되므로 =>  BFS(큐)
+
+
+def solution(n, computers):
+    
+    answer = 0
+    visited = [0]*n #방문노드
+    bfs = []
+    print(visited)
+    
+    while 0 in visited :
+        print("테스트중")
+        
+        start = visited.index(0)
+        bfs.append(start)
+        visited[start] = 1 
+        
+        while bfs : #큐에 값이 존재하면 계속 실행
+            node = bfs.pop(0)
+            for i in range(n) :
+                if visited[i] == 0 and computers[node][i] == 1:
+                    bfs.append(i)
+                    visited[i] = 1 
+                    #재귀적으로 풀어야함..? 
+                
+        answer +=1        
+                
+    return answer
+
 =============================================================
 #1차시도
 #정확성 실패
