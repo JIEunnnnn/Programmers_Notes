fd2be1c7d4795314bc31665d86be095301483dc4
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-08 16:00:32 +0900

    Update 여행경로.py

--- DFS&BFS/여행경로.py
+++ DFS&BFS/여행경로.py
@@ -1,4 +1,35 @@
+#LV.3 여행경로
+#주어진리스트에 따라 경로그리기
+#
+# 1. 딕셔너리에 여러개값을 병합하는 방법...알게됨
+# 2. 리스트를 스택으로 구현하는 방법 top = stack[-1]
 
+def solution(tickets):
+    routes = {} #시작경로-도착경로
+    
+    for i in tickets :
+        routes[i[0]] = routes.get(i[0], []) + [i[1]]
+        #get(key, default) 키값이 존재하지않으면 default값을 가져온다.
+        #딕셔너리 1개키값에 여러 값 추가하는방법... 시발        
+    print(routes) 
+    #	{'ICN': ['SFO', 'ATL'], 'SFO': ['ATL'], 'ATL': ['ICN', 'SFO']}   
+    
+    for r in routes :
+        routes[r].sort(reverse=True)
+        
+    stack = ["ICN",] 
+    path = []
+    
+    while len(stack) > 0 :
+        top = stack[-1] #리스트를 스택으로 구현하는 방법
+        if top not in routes or len(routes[top]) == 0 :
+        #routes에 키가 존재하지않거나, 키는존재하나 값이없을경우
+            path.append(stack.pop())
+        else :
+            stack.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+        
+    return path[::-1] #정렬
 
 
 =====================================================
