23944ac9f38bde6384296d4c2a37ae97b51f8cb8
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-08 21:45:53 +0900

    Update 네트워크.py

--- DFS&BFS/네트워크.py
+++ DFS&BFS/네트워크.py
@@ -41,13 +41,9 @@ def solution(n, computers):
     sum = 0
     
     for i,v in enumerate(computers) : #index
-        for j,k in enumerate(v) :
+        for j,k in enumerate(v) : 
             if i != j :
                 sum += k
                 
-
-    #print(sum) 
-    #print(int(sum/2))
     answer =  n - math.ceil(sum/2) 
-    #answer = 0
     return answer
