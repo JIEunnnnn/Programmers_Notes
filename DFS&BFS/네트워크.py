648371118857a69b6323401555ac1f10d9bc413a
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-08 21:45:02 +0900

    Update 네트워크.py

--- DFS&BFS/네트워크.py
+++ DFS&BFS/네트워크.py
@@ -1,31 +1,32 @@
-# 리트코드 200 
-
+#LV.3 네트워크 
+#주어진리스트에 따라 연결해야할 네트워크 수 구하기
+#
 #경로만 알면 되므로 =>  BFS(큐)
 
+#리트코드 200 
 
 def solution(n, computers):
     
     answer = 0
-    visited = [0]*n #방문노드
-    bfs = []
+    visited = [0]*n #방문노드(방문하면=1)
+    bfs = [] #경로노드(큐)
     print(visited)
     
-    while 0 in visited :
-        print("테스트중")
+    while 0 in visited : #방문하지않는 노드가 존재하지않을때까지 
         
-        start = visited.index(0)
-        bfs.append(start)
+        start = visited.index(0) #리스트의 value(0)통해 KEY찾기
+        bfs.append(start) #시작노드 
         visited[start] = 1 
         
-        while bfs : #큐에 값이 존재하면 계속 실행
+        while bfs : #큐(bfs)에 값이 존재하면 계속 실행
             node = bfs.pop(0)
             for i in range(n) :
                 if visited[i] == 0 and computers[node][i] == 1:
-                    bfs.append(i)
-                    visited[i] = 1 
-                    #재귀적으로 풀어야함..? 
+                #방문하지않은 노드이며 연결되어있을 경우
+                    bfs.append(i)  #방문한 노드부터 다시시작
+                    visited[i] = 1 #방문했다
                 
-        answer +=1        
+        answer +=1 #큐에 값이없이없을경우 +1 ==> 연결된 노드 다찾았으며 다시 방문하지않은 노드부터 연결고리 찾기       
                 
     return answer
 
