e46d9297ae8fcb263cb00452f1ad9ce0bbec5e03
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-09 21:04:03 +0900

    Create 가운데글자가져오기.py

---
+++ LV.1/가운데글자가져오기.py
@@ -0,0 +1,18 @@
+#LV.1 가운데글자 가져오기
+#주어진 문자열이 홀수냐 짝수냐에 따라 가운데글자 가져오기 
+
+import math 
+
+def solution(s):
+    index = 0
+    if len(s)%2 == 0 :
+        #짝수일경우 
+        index = len(s)-1
+        index = int(index/2)
+        print(index)
+        return s[index :index+2]
+        
+    else :
+        index = int(len(s)/2)
+        print(index)
+        return s[index]
