6c5cf12735bd46c7b6c22a757a4264c7a8fe5923
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-09 17:33:44 +0900

    Update 단어변환.py

--- DFS&BFS/단어변환.py
+++ DFS&BFS/단어변환.py
@@ -1,7 +1,39 @@
+#2차시도 
 
+#테스트케이스는 맞췄으나 정확성오류
+def solution(begin, target, words):
+    if target not in words :
+        print("없음")
+        return 0 
+    
+    list_begin = begin[:]
+    list_target = target[:]
+    err = 0
+    for i,v in enumerate(list_target) :
+        
+        if list_begin[i] != list_target[i] :
+            err += 1
+            
+    dict_list = {}
+    
+    for i in words :
+        tmp = i[:]
+        count = 0
+        for j,v in enumerate(list_target) :
+            if tmp[j] == list_target[j] :
+                count +=1 
+        dict_list[count] = dict_list.get(count,'' ) + i + ","
+        #routes[i[0]] = routes.get(i[0], []) + [i[1]]
+    
+    print(len(dict_list))
+    print(dict_list)
+    
+    answer = len(dict_list) +1 
+    if err == 1 :
+        answer = 1
+    return answer
 ====================================================
 #1차시도
-
 
 #https://rfriend.tistory.com/327 문자열관련메소드
 
