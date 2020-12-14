4ce16a292d939ad1646aa0feebbe17e4935669ca
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-14 17:59:48 +0900

    Update 큰수만들기.py

--- Greedy/큰수만들기.py
+++ Greedy/큰수만들기.py
@@ -1,1 +1,18 @@
 
+#1차시도
+from itertools import permutations
+
+def solution(number, k):
+    size = len(number) - k
+    number = list(number)
+    print(number)
+    
+    #list2 = list(permutations(number,size))
+    list2 = list(map(''.join, permutations(number,size) ))
+    #print(list(list2))
+    list3 = list2.sort(reverse=True)
+    answer = list3[0]
+    print(answer)
+    
+    #answer = ''
+    return answer
