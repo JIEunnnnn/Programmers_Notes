c8772e26d86dbc00465888fac30a759310090967
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-15 13:58:46 +0900

    Update 구명보트.py

--- Greedy/구명보트.py
+++ Greedy/구명보트.py
@@ -1,3 +1,21 @@
+#LV.2 구명보트
+#people 리스트를 옮기는데 걸리는 최소한의 횟수
+#
+#시도는 거의 유사했으나 pop()으로 인해 시간초과발생함..!
+#반드시 pop()이 필요하지않는경우에는 쓰지말자 => 효율성문제!!! 
+
+def solution(people, limit):
+    answer = len(people)
+    p = sorted(people,reverse = True)
+    s,e = 0, len(p)-1
+    while s < e : 
+        if p[s]+p[e] <= limit :
+            e-=1
+            answer-=1
+        s+=1
+    return answer
+
+=========================================================
 #1차시도
 #효율성 및 정확성... ㅠ 
 
