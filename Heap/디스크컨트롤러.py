8088193000c50de74e33364f20be709975066ab4
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-15 15:16:55 +0900

    Create 디스크컨트롤러.py

---
+++ Heap/디스크컨트롤러.py
@@ -0,0 +1,25 @@
+#LV.3 디스크컨트롤러
+#소요시간을 최소화하여 반환하기 
+#
+#
+#
+
+def solution(jobs):
+    answer = 0
+    start = 0  # 현재까지 진행된 작업 시간
+    length = len(jobs)
+
+    jobs = sorted(jobs, key=lambda x: x[1])  # 소요시간을 오름차순으로 정렬하기
+
+    while len(jobs) != 0:
+        for i in range(len(jobs)):
+            if jobs[i][0] <= start:
+                start += jobs[i][1]
+                answer += start - jobs[i][0]
+                jobs.pop(i)
+                break
+            # 해당시점에 아직 작업이 들어오지 않았으면 시간 ++
+            if i == len(jobs) - 1:
+                start += 1
+
+    return answer // length
