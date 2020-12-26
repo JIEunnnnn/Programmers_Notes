e0b591ad22330d087ea1fad35df50173ac0ac71d
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2020-12-26 14:44:10 +0900

    Update 행렬의 곱셈.py

--- 기타연습문제/행렬의 곱셈.py
+++ 기타연습문제/행렬의 곱셈.py
@@ -1,3 +1,22 @@
+#LV.2 행렬의 곱셈 
+#2개의 행렬을 곱한값을 출력하는 알고리즘
+#
+#처음에 아무생각없이 tmp를 둬서 문제를 풀었는데, 2*3, 3*2 같은 행렬을 생각하지않음ㅠ 
+#행렬의곱셈 규칙을 생각해서 문제를 풀었다! 
+#arr1 : [[1, 2, 3], [4, 5, 6]], arr2 : [[1, 4], [2, 5], [3, 6]] return : [[14, 32], [32, 77]]
+
+def solution(arr1, arr2):
+    #print(len(arr2))
+    #print(len(arr2[0]))
+    answer = [[0 for _ in range(len(arr2[0]))] for _ in range(len(arr1))]
+    print(answer)
+    
+    for i in range(len(arr1)) :
+        for j in range(len(arr2[0])) :
+            for k in range(len(arr2)) :
+                answer[i][j] += (arr1[i][k] * arr2[k][j])
+    return answer
+
 
 ======================================================================
 #1차시도 => 정확성 0점...
