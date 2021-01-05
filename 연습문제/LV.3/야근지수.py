4e61531b49dfa081fbdf3b2b3b61fbe974f3f0da
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-01-05 14:09:49 +0900

    Create 야근지수.py

---
+++ 연습문제/LV.3/야근지수.py
@@ -0,0 +1,33 @@
+========================================================================
+#1차시도
+#정확성및 효율성...최악^^7 
+
+def solution(n, works):
+    
+    while n != 0 :
+    
+        if n >= len(works) :
+            for i,v in enumerate(works) :
+                works[i] = v-1
+            n -= len(works)
+            
+        
+        else :
+            if max(works) >= n :
+                idx = works.index(max(works))
+                works[idx] = works[idx] - n
+                n=0
+            elif max(works) < n and max(works) > 0 :
+                idx = works.index(max(works))
+                works[idx] = works[idx] - n
+                n-=works[idx]
+                
+            
+            else : #max==0일경우 
+                return 0
+                
+    print(works)
+    answer = 0
+    for i in works :
+        answer += i**2
+    return answer
