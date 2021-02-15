dc175cc62c1c8d4dec165d561fda566671fcb52f
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-02-15 13:24:50 +0900

    Update 단어퍼즐.py

--- 연습문제/2017 팁스타운/단어퍼즐.py
+++ 연습문제/2017 팁스타운/단어퍼즐.py
@@ -1,7 +1,36 @@
 
-====================================================================
-#1차시도
 
+====================================================================
+#2차시도 => 이렇게 하는게 맞는지 막막하다.... 
+
+def solution(strs, t):
+    answer = 0
+    
+    print(list(t))
+    t_list = list(t)
+    tmp = ''
+    stack = []
+    
+    
+    while t_list :
+        tmp += t_list[0]
+        print(tmp)
+        
+        if tmp in strs :
+            print("테스트")
+            stack.append(tmp)
+            
+            print(tmp)
+        
+        t_list.pop(0)
+
+
+    
+
+    return answer
+
+
+#1차시도 => 중복허용 조합을 일으키면... 안되지..효율성극악..
 from itertools import combinations_with_replacement
 	["ab", "na", "n", "a", "bn"], "nabnabn"
 def solution(strs, t):
