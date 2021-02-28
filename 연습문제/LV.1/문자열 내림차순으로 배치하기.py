26e13a2135db55ecd01a2e0458ace9bc5750ab1e
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-02-28 21:57:20 +0900

    Create 문자열 내림차순으로 배치하기.py

---
+++ 연습문제/LV.1/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,16 @@
+#LV.1 문자열내림차순으로 배치하기
+#주어진문자열을 내림차순으로 배치
+#
+#문자열 => 리스트 변경하여 sort시킨 후 join으로 문자열복구
+#
+
+def solution(s):
+    
+    tmp = []
+    for i in s :
+        tmp.append(i)
+    
+    tmp.sort(reverse=True)
+    #print(tmp)
+    
+    return ''.join(tmp)
