36d19e3429e80263ce560169a4f4f24447ec0e29
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-02-28 20:52:11 +0900

    Update 소수 찾기.py

--- 연습문제/LV.1/소수 찾기.py
+++ 연습문제/LV.1/소수 찾기.py
@@ -1,8 +1,35 @@
 #LV.1 소수찾기
 #1부터 N까지 중에서 소수 몇개인지를 찾는 문제
 #
-#
-#
+#전체 for문돌리면 시간초과 발생.. => 제곱근 개념 활용해서 효율성문제 해결
+#https://wikidocs.net/21638  (소수판별참고페이지)
+
+
+import math
+
+def is_prime_number(x) :
+    array = [1 for i in range(x)]
+    array[0] = 0 #1은 소수가 아니므로 0 으로 설정 
+    
+    #print(array)
+    m = int(math.sqrt(x)) #제곱근까지 검사
+     
+    for i in range(2, m+1):
+        
+        if array[i-1] == 1 : #소수일경우 
+            for j in range(i+i, x+1, i) : # 소수의 배수들은 배수로 설정! (ex, 2일경우 4, 6, 8 , 10 )
+                                          # 주어진 숫자 X까지 i 배수 검사!!
+                
+                    array[j-1] = 0
+                    
+    #print(array)
+    return sum(array)
+    
+
+def solution(n):
+    
+    answer = is_prime_number(n)
+    return answer
 
 
 ======================================================================
