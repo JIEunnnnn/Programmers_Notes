157bc4227a2c5a206b0d60371852f0bba28bf55d
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-02 15:07:41 +0900

    Update 입양 시각 구하기(1).sql

--- SQL/GROUB BY/입양 시각 구하기(1).sql
+++ SQL/GROUB BY/입양 시각 구하기(1).sql
@@ -1,4 +1,4 @@
---그룹화 및 조건절 추가--
+#그룹화 및 조건절 추가
 
 SELECT HOUR(DATETIME) AS HOUR, COUNT(HOUR(DATETIME)) AS COUNT
 FROM ANIMAL_OUTS
@@ -6,7 +6,7 @@ WHERE HOUR(DATETIME) >= 9 AND HOUR(DATETIME) <=19
 GROUP BY HOUR(DATETIME)
 ORDER BY HOUR(DATETIME) ASC
 
-------------------OR--------------------
+----------------OR-----------------
                                           
 SELECT HOUR(DATETIME) AS HOUR, COUNT(HOUR(DATETIME)) AS COUNT
 FROM ANIMAL_OUTS
