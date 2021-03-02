6b7824e831e99037fa7bd001150c30a94dd50f9d
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-02 19:49:54 +0900

    Create 정수 내림차순으로 배치하기.py

---
+++ 연습문제/LV.1/정수 내림차순으로 배치하기.py
@@ -0,0 +1,20 @@
+#LV.1 정수 내림차순으로 배치하기.py
+#주어진 int를 뒤짚어 출력하는 알고리즘
+#
+#문자열변환 후, sorted 함수 이용 => join 및 int함수로 변환 
+#
+
+def solution(n):
+    
+    answer = []
+    for i in str(n) :
+        answer.append(i)
+        
+    #print(answer)
+    #print(sorted(answer, key= lambda x : x[0], reverse=True))
+    
+    answered = sorted(answer, key= lambda x : x[0], reverse=True)
+    
+    return int(''.join(answered))
+    
+    
