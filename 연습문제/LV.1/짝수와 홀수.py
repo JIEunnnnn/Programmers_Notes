e68b5e070930d190af3bffdb312889991361e9fd
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-02 20:23:54 +0900

    Update 짝수와 홀수.py

--- 연습문제/LV.1/짝수와 홀수.py
+++ 연습문제/LV.1/짝수와 홀수.py
@@ -10,5 +10,10 @@ def solution(num):
         return "Even"
     else :
         return "Odd"
-        
+      
+#불(bool) 자료형 : 참(True)과 거짓(False)의 두 가지 값을 가지고 있는 자료형
+
+        #   참 : 1 , 비어있지 않은 문자열,리스트, 튜플 딕셔너리 등등 (ex. python, [1,2,3])
+        #   거짓 : 0, None, 비어있는 문자열, 리스트, 튜플, 딕셔너리 등 ("", [], (), {})
+        #   if num % 2 : return "Odd" 
         
