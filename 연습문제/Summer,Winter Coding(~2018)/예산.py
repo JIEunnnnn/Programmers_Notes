106474396684a8dc0dc0e11d1804c93f1f24c128
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-05 14:00:42 +0900

    Update 예산.py

--- 연습문제/Summer,Winter Coding(~2018)/예산.py
+++ 연습문제/Summer,Winter Coding(~2018)/예산.py
@@ -1,8 +1,23 @@
 #Summer,Winter Coding(~2018) 예산
+#주어진 리스트에서 예산내로 최대몇개의 부서를 지원해줄수있는가?
 #
-#
-#
-#
+#최대로 지원해줄수있는 횟수를 구해야하므로, 오름차순 정렬해서 가장 작은숫자부터..구현! 
+#너무 어렵게 생각했나보다...ㅎㅎ;
+
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    
+    for cost in d :
+        budget -= cost
+        answer += 1
+        
+        if budget < 0: #예산초과할경우 -1
+            answer -=1 
+            break
+            
+    return answer
+
 
 #시간초과발생
 def solution(d, budget):
