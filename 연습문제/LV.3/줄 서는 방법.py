59d7de5c86997e385b04ab5c0e4de1fd9017c3bc
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-03-11 18:52:35 +0900

    Update 줄 서는 방법.py

--- 연습문제/LV.3/줄 서는 방법.py
+++ 연습문제/LV.3/줄 서는 방법.py
@@ -1,3 +1,35 @@
+#LV.3 줄서는방법
+#주어진 N명의 사람들을 k번째로 줄세울때 나오는 리스트 
+#
+#대박적..두달만에 풀어보니 성공...
+#재귀를 활용해야하는데 함수정의하기 헷갈려서 while 문 활용했다 :)
+
+import math
+
+
+def solution(n, k):
+    answer = []
+    
+    people = [i for i in range(1,n+1)]
+    
+    while k != 1 :
+        fac = math.factorial(len(people))
+        check = fac//len(people) 
+        idx = 0
+        for i in range(n):
+            if k <= (i+1)*check :
+                idx = i 
+                k -= (i*check)
+                break 
+    
+        answer.append(people[idx])
+        people.pop(idx)
+        
+        if k == 1 :
+            answer.extend(people)
+    
+    
+    return answer
 
 =================================================================
 #1차시도
