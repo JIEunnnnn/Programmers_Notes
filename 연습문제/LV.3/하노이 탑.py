9f6d47abfa987135383eedcea39ba73fb4a194c4
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-05-03 21:52:08 +0900

    Create 하노이 탑.py

---
+++ 연습문제/LV.3/하노이 탑.py
@@ -0,0 +1,21 @@
+#LV.3 하노이 탑
+#n개의 원판을 3번 원판으로 최소로 옮기는 방법
+#
+#
+#재귀를 활용하여, 알고리즘 구현해야함!! 
+
+def solution(n):
+    answer = [] 
+    
+    def hanoi(n, start, end, assist):
+        nonlocal answer
+        if n == 1: # 원판이 1개면 재귀 종료
+            answer.append([start, end])
+            return
+        hanoi(n-1, start, assist, end) 
+        answer.append([start, end])
+        hanoi(n-1, assist, end, start)
+        
+    hanoi(n, 1, 3, 2) 
+    return answer
+  
