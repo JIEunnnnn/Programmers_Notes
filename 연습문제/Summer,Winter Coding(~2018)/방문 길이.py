eb81cda0cb0c9a4462ce4b0ab8353ba2a6e92816
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-05-05 14:49:18 +0900

    Update 방문 길이.py

--- 연습문제/Summer,Winter Coding(~2018)/방문 길이.py
+++ 연습문제/Summer,Winter Coding(~2018)/방문 길이.py
@@ -1,8 +1,30 @@
 #Summer,Winter Coding(~2018) 방문길이
+#명령어에 따른, 이동거리 구하기 
 #
-#
-#
-#
+#최초로가는 길만 세기 + 지도밖으로 나가지않기! 
+#백준으로 훈련해서 쉽게 풀수있었던 문제!
+
+def solution(dirs):
+    
+    goto = {'U' : (0,1), 'D' : (0,-1), 'R' : (1,0), 'L' : (-1,0)}
+    
+    x, y = 0, 0 
+    cnt = 0
+    stack = []
+    
+    for d in dirs :
+        tx, ty = goto[d]
+        dx, dy = tx + x, ty + y 
+        if -5 <= dx <= 5 and  -5 <= dy <= 5 :
+            if (x,y,dx,dy) not in stack and (dx,dy,x,y) not in stack :
+                cnt +=1 
+                stack.append((x,y,dx,dy))
+            
+            x,y = dx, dy
+    
+    
+    return cnt 
+====================================================================
 
 #1차시도 => 뭐가문제지..? 
 def solution(dirs):
