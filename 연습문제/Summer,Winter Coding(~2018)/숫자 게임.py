b8b82b4aba11213639336f8fa193db6d7bd3a462
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-05-06 13:52:14 +0900

    Update 숫자 게임.py

--- 연습문제/Summer,Winter Coding(~2018)/숫자 게임.py
+++ 연습문제/Summer,Winter Coding(~2018)/숫자 게임.py
@@ -27,7 +27,9 @@ def solution(A, B):
     A.sort()
     B.sort()
     
-    for i,v in enumerate(A) :
+    for i,v in enumerate(A) : # 순서에 맞출필요가 없죠.. 
+                              # 왜냐하면, B < A일경우 다음 A순서에서는 B가 더 클 가능성을 염두해야하니까 
+                              # 결국 B가 많이 이겨야하니까, 질경우 제일 작은수로 배치하면 되므로.. 이기는 경우만 생각ㄱㄱ
         if v < B[i] :
             answer += 1 
     
