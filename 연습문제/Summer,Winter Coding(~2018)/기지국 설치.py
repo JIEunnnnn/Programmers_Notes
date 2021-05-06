c49f198608045eddf0dad8fd8f21b1ffe4cc3bf9
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-05-06 15:00:18 +0900

    Update 기지국 설치.py

--- 연습문제/Summer,Winter Coding(~2018)/기지국 설치.py
+++ 연습문제/Summer,Winter Coding(~2018)/기지국 설치.py
@@ -4,6 +4,7 @@
 #
 #
 
+#1차시도 
 def solution(n, stations, w):
     answer = 3
     
@@ -20,10 +21,24 @@ def solution(n, stations, w):
         del apart[apart.index(s)]
         
     print(apart)
+    w_chk = [i for i in range(1, 2 * w + 2 )]
+    print(w_chk)    
     
+    cnt = 1
+    chk = []
     for a in apart :
-        
+        #print(w_chk)
+        if a not in w_chk :
+            cnt +=1 
+            abc = 0
+            for i,v in enumerate(w_chk) :
+                w_chk[i] = a + abc 
+                abc +=1 
     
+    #print(cnt)
+    #print(chk)
+    
+    return cnt 
     
     
     
