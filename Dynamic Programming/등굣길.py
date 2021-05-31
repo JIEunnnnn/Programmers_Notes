7ff188cb9819c4e60cdffab37f11fa83dbc9373d
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-05-31 17:09:38 +0900

    Update 등굣길.py

--- Dynamic Programming/등굣길.py
+++ Dynamic Programming/등굣길.py
@@ -1,3 +1,26 @@
+#LV.3 등굣길
+#최단거리를 찾는 문제
+#
+#처음에 당연히 DFS! 생각을 했는데
+#DP이면 점화식이라는 사실을 인지하지 못했다...!!! 
+#https://m.post.naver.com/viewer/postView.nhn?volumeNo=26933342&memberNo=33264526
+
+def solution(m, n, puddles):
+    answer = 0
+    
+    maps = [[0 for i in range(m)] for i in range(n)]
+    maps[0][0] = 1
+    
+    for x in range(n) :
+        for y in range(m) :
+            if x == 0 and y == 0 :
+                continue 
+            if [y+1,x+1] in puddles :
+                maps[x][y] = 0 
+            else :
+                maps[x][y] += (maps[x-1][y] + maps[x][y-1])
+                
+    return (maps[-1][-1]) % 1000000007
 
 ===================================================
 #1차시도
