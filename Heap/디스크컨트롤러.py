1e3601cd05f89fe362f7df347c69c60aebdc172e
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-06-18 17:22:29 +0900

    Update 디스크컨트롤러.py

--- Heap/디스크컨트롤러.py
+++ Heap/디스크컨트롤러.py
@@ -2,7 +2,37 @@
 #소요시간을 최소화하여 반환하기 
 #
 #힙구조로만 생각했는데, 2중포문돌려서 start를 더함으로써 구하는 방식도 있다는걸알게됨...ㄷㄷ 
-#
+#heapq 와 deque를 활용한 문풀....;;;
+
+from collections import deque 
+import heapq 
+
+def solution(jobs):
+    answer = 0
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs ], key= lambda x : (x[1], x[0])))
+    q= []
+    
+    heapq.heappush(q, tasks.popleft())
+    current_time, total_response_time = 0,0
+    while len(q) > 0 :
+        dur, arr = heapq.heappop(q)
+        current_time = max(current_time + dur, arr + dur)
+        # 현재시간 + 실행시간 OR 요청시간 + 실행시간
+        # 현재시간이 요청시간보다 더 클경우 뺄수없으므로 max !!! 
+        
+        total_response_time += current_time - arr
+        # 요청 전 시간 빼기
+        
+        while len(tasks) > 0 and tasks[0][1] <= current_time:
+            # 현재시간보다 전의 작업들이 존재할때! 
+            heapq.heappush(q, tasks.popleft())
+            
+        if len(tasks) > 0 and len(q) == 0:
+            # 현재시간보다는 늦게 있지만, q가 존재하지않을때 (예외처리)
+            heapq.heappush(q, tasks.popleft())
+            
+    return total_response_time // len(jobs)
+
 
 def solution(jobs):
     answer = 0
