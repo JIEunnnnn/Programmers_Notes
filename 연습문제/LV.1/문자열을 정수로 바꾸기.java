59a9e089ab24f34f1c07de7f4d023f8a27264cef
JIEUN MOON <44091725+JIEunnnnn@users.noreply.github.com>
2021-08-08 17:40:39 +0900

    Create 문자열을 정수로 바꾸기.java

---
+++ 연습문제/LV.1/문자열을 정수로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        //System.out.println(s);
+        answer = Integer.parseInt(s);
+        //System.out.println(answer);
+        
+        return answer;
+    }
+}
